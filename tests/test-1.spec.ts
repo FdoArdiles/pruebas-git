0e1ee288c1f46b4f5e9c013469728e86bbbea892
FdoArdiles <fernando_figueroa16@hotmail.com>
2026-05-11 17:23:20 -0400

    update test-1

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -5,7 +5,7 @@ test('test', async ({ page }) => {
  await page.getByRole('link', { name: 'Elements' }).click();
  await page.getByRole('link', { name: 'Check Box' }).click();
  await page.goto('https://demoqa.com/checkbox');
- await page.getByRole('checkbox', { name: 'Select Home' }).click();
+ await page.locator('//*[@id="root"]/div/div/div/div[2]/div[1]/div/div[3]/div/div/div/div[1]/span[3]').click()
  const resultado = await page.locator('#result');
  expect(resultado).toHaveText("You have selected :homedesktopdocumentsdownloadsnotescommandsworkspaceofficewordFileexcelFilereactangularveupublicprivateclassifiedgeneral");
 });
