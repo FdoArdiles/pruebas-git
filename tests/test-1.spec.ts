64dda06c2a0deecf2c20ae7c4f41afd3611adb65
FdoArdiles <fernando_figueroa16@hotmail.com>
2026-05-11 17:42:09 -0400

    update tobechecked

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -5,7 +5,9 @@ test('test', async ({ page }) => {
  await page.getByRole('link', { name: 'Elements' }).click();
  await page.getByRole('link', { name: 'Check Box' }).click();
  await page.goto('https://demoqa.com/checkbox');
- await page.locator('//*[@id="root"]/div/div/div/div[2]/div[1]/div/div[3]/div/div/div/div[1]/span[3]').click()
- const resultado = await page.locator('#result');
- expect(resultado).toHaveText("You have selected :homedesktopdocumentsdownloadsnotescommandsworkspaceofficewordFileexcelFilereactangularveupublicprivateclassifiedgeneral");
+ const checkboxhome = page.locator('//*[@id="root"]/div/div/div/div[2]/div[1]/div/div[3]/div/div/div/div[1]/span[3]')
+ await checkboxhome.check();
+ expect(checkboxhome).toBeChecked();
+ const resultado = page.locator('#result');
+ await expect(resultado).toHaveText("You have selected :homedesktopdocumentsdownloadsnotescommandsworkspaceofficewordFileexcelFilereactangularveupublicprivateclassifiedgeneral")
 });
